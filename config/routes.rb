f660a6bad933d35a8208bb186cca5b80953165d7
r_kanamaru <ryouji0623.k@gmail.com>
2018-11-06 23:38:53 +0900

    add_view_file_for_group_new&edit
    groupのnew＆editのビューファイルを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'groups#index'
-  resources :users, only: [:index,:edit,:update]
-  resources :groups, only: [:new,:create,:edit,:update] do
-   resources :messages, only: [:index,:create]
+  root 'messages#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+   resources :messages, only: [:index, :create]
  end
 
 end
