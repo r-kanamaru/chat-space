3b4e8dba5b70413fe300727e3fb42534d7e2855b
r_kanamaru <ryouji0623.k@gmail.com>
2018-11-06 22:23:58 +0900

    add_controller_action
    コントローラーにアクションを定義した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,23 @@
 class UsersController < ApplicationController
 
- def edit
- end
+def index
+end
 
- def update
+def edit
+end
+
+def update
   if current_user.update(user_params)
     redirect_to root_path
   else
     render :edit
   end
- end
+end
 
 private
 
- def user_params
+def user_params
   params.require(:user).permit(:name, :email)
- end
+end
 
 end
