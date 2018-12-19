7b5c8a0b1d882f008c5c7a35ee64d4cb2613427f
r_kanamaru <ryouji0623.k@gmail.com>
2018-12-19 22:56:35 +0900

    uum...

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,5 +14,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
 end
 
