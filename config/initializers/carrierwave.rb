9796bbeaf5133fbbfc6b207b0f86e79dd0fbf07e
r_kanamaru <ryouji0623.k@gmail.com>
2018-12-20 21:55:18 +0900

    aws_s3_bugget_update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'upload-test-for-chat-space'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test-for-chat-space'
+  config.fog_directory  = 'chat-space-image-upload-test'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-image-upload-test'
 end
