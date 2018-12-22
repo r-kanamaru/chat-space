57a4702bc5d6e90fca12bc8673debc363039aef1
r_kanamaru <ryouji0623.k@gmail.com>
2018-12-22 15:18:31 +0900

    roll_back

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,12 +7,13 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'chat-space-image-upload-test'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-image-upload-test'
 end
+
 
